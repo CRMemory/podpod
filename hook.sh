b81992246c8c2312163fb50e05fe2b5c61ca4ef2
Memroy <28638530+MasterOfMemroy@users.noreply.github.com>
2017-10-05 23:04:46 +0200

    Added a comment to the sample hook script

--- hook.sh
+++ hook.sh
@@ -1,1 +1,6 @@
+# This is an example hook file. It can be used for pre or postprocessing
+# podcasts.
+# I originally wrote it to update my mpd database after every run. Maybe
+# you find other clever uses.
+
 echo "Hook executed"
